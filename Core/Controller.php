f3a438aca5c0306003007a76effa84e64b3c1251
Mykyta Shelest <shelestmykyta2022@gmail.com>
2022-10-13 23:59:54 +0300

    implemented View class and layouts/views

--- Core/Controller.php
+++ Core/Controller.php
@@ -2,12 +2,22 @@
 
 namespace Core;
 
+use Core\View;
+
 abstract class Controller
 {
     protected array $route;
+    public View $view;
 
     public function __construct(array $route)
     {
         $this->route = $route;
+        $this->view = new View($route);
+        $this->setLayout();
+    }
+
+    protected function setLayout(): void
+    {
+        $this->view->setLayout('default');
     }
 }
--- lib/Messages.php
+++ lib/Messages.php
@@ -7,4 +7,6 @@ class Messages
     const ROUTE_NOT_FOUND = 'Page not found';
     const CONTROLLER_NOT_FOUND = 'Controller not found';
     const ACTION_NOT_FOUND = 'Action not found';
+    const VIEW_NOT_FOUND = 'View not found';
+    const LAYOUT_NOT_FOUND = 'Layout not found';
 }
