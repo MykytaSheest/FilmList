b2dbf74898683c95a9d4d5159f29c0b92dd9a22c
Mykyta Shelest <shelestmykyta2022@gmail.com>
2022-10-16 16:41:52 +0300

    created model class

---
+++ Core/Model.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core;
+
+abstract class Model
+{
+    protected DB $db;
+
+    public function __construct()
+    {
+        $this->db = new DB();
+    }
+
+    /** @var int */
+    protected $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function __set(string $name, $value)
+    {
+        $camelCaseName = $this->underscoreToCamelCase($name);
+        $this->$camelCaseName = $value;
+    }
+
+    private function underscoreToCamelCase(string $source): string
+    {
+        return lcfirst(str_replace('_', '', ucwords($source, '_')));
+    }
+
+    /**
+     * @return static[]
+     */
+    public function findAll()
+    {
+        return $this->db->row('SELECT * FROM' . $this->getTableName() . ';');
+    }
+
+    abstract protected function getTableName(): string;
+}
