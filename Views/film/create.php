4ad6dac53a72d9d7517cd6a4aa823a15c4b5fa05
Mykyta Shelest <shelestmykyta2022@gmail.com>
2022-10-19 18:38:27 +0300

    add validation to year fild

--- Views/film/create.php
+++ Views/film/create.php
@@ -1,7 +1,7 @@
 <script src="../../public/js/add_film.js" defer></script>
 <form action="<?= getHost()?>/post/create" METHOD="post" class="form-add-film">
     <input type="text" name="title" class="film-input title" placeholder="Film title..." required>
-    <input type="number" name="year" class="film-input year" placeholder="Film year..." min="1870" required>
+    <input type="number" title="You can choose years between 1850 and 2022" name="year" class="film-input year"  placeholder="Film year between 1850 and 2022..." min="1850" max="2022"  required>
     <select name="format" class="format">
         <option value="1">VHS</option>
         <option value="2">DVD</option>
